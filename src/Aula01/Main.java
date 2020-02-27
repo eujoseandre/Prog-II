051f366947e38f0e9feab6f3c2943d1a31ee8af6
Jose Andre <eujoseandre@gmail.com>
2020-02-27 16:37:25 -0300

    Commit inicial

---
+++ src/Aula01/Main.java
@@ -0,0 +1,26 @@
+
+package Aula01;
+
+public class Main {
+
+    public static void main(String[] arguments) {
+        
+        Pessoa andre = new Pessoa("André", "Souza", 19, 1.70);
+        andre.imprimir();
+        
+        Pessoa eduardo = new Pessoa();
+        
+        eduardo.setNome("Eduardo");
+        eduardo.setIdade(19);
+        eduardo.setAltura(1.70);
+        eduardo.setSobrenome("Weber");
+        eduardo.imprimir(); 
+        
+        Pessoa maria = new Pessoa();
+        maria.setNome("Maria");
+        maria.setSobrenome("Weasley");
+        maria.setIdade(25);
+        maria.setAltura(1.80);
+        maria.imprimir();
+    }  
+}
